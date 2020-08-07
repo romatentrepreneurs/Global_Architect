043ec870e5ff1ab83c4b2c30e474f3c4ad133dc0
romatentrepreneurs <69040210+romatentrepreneurs@users.noreply.github.com>
2020-08-07 19:08:41 +0530

    Update map.js

--- js/map.js
+++ js/map.js
@@ -3,10 +3,10 @@ function initialize() {
 		zoom: 15,
 		center: new google.maps.LatLng(9.7868064, 76.6092692), //change the coordinates
 		mapTypeId: google.maps.MapTypeId.ROADMAP,
-		scrollwheel: false,
-		mapTypeControl: false,
-		zoomControl: false,
-		streetViewControl: false
+		scrollwheel: true,
+		mapTypeControl: true,
+		zoomControl: true,
+		streetViewControl: true
 	};
 	var map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
 	var marker = new google.maps.Marker({
